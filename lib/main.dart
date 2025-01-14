951773c74d58e834336cceb129b796d971622c6a
SaulSandovalM <sauldevelop@gmail.com>
2025-01-14 01:28:09 -0600

    Lanzamiento de la primera versión del portafolio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:portafolio/providers/conferences_provider.dart';
 import 'package:portafolio/providers/courses_provider.dart';
@@ -9,6 +10,7 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
