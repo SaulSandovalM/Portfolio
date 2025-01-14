f03f3952dbad5b5b90bbbf676a0c6d2bdb965a05
SaulSandovalM <sauldevelop@gmail.com>
2025-01-14 01:03:45 -0600

    again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1284261695"
+    serviceWorkerVersion: "1806542970"
   }
 });
