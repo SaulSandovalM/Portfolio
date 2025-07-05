94b60d38acc6d6c3af5d609ef2c32693676a1bc7
SaulSandovalM <SaulSandovalM@users.noreply.github.com>
2025-07-05 00:08:32 +0000

    deploy: 6f1fcf6722e730fcdd150e7e8e9a0b76aec91527

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "493485554"
+    serviceWorkerVersion: "3524124526"
   }
 });
